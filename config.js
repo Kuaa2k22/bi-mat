6620a6a7be91ab6a6f275039a0c00989ce8b3241
Kuaa2k22 <85273864+Kuaa2k22@users.noreply.github.com>
2021-06-03 21:32:11 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Trang à!',
+    introTitle: 'Các bạn nữ trong nhóm à!',
     introDesc: `Trứng rán cần mỡ, bắp cần bơ,
-    yêu không cần cớ cần Trang cơ `,
+    yêu không cần cớ cần các cậu  cơ `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Anh em xê ra hết nhaaaaaa 😙',
+    desc: 'Các bạn nữ là của tôi nhasaaaaaa <3 ',
+    btnYes: 'Các iem là của anh  <33',
     btnNo: 'Không chúng ta chỉ là bạn bình thường :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Yêu em <3.',
+    messDesc: 'Yêu các em <3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://https://m.me/kuaa2k22' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
